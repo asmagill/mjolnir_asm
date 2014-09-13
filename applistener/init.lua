1f3c1446292b7c8dc34b23c0d5558ebe6b737a7b
asmagill <asmagill@users.noreply.github.com>
2014-09-12 20:33:54 -0500

    initial applistener

---
+++ applistener/init.lua
@@ -0,0 +1,56 @@
+local module = {
+--[=[
+    _NAME        = 'mjolnir._asm.applistener',
+    _VERSION     = 'the 1st digit of Pi/0',
+    _URL         = 'https://github.com/asmagill/mjolnir_asm.applistener',
+    _LICENSE     = [[ See README.md ]]
+    _DESCRIPTION = [[
+    
+--- === mjolnir._asm.applistener ===
+---
+--- Home: https://github.com/asmagill/mjolnir_asm.applistener
+---
+--- Listen to notifications sent by other apps, and maybe send some yourself.
+---
+--- This module is based primarily on code from the previous incarnation of Mjolnir by [Steven Degutis](https://github.com/sdegutis/).
+
+    ]],
+--]=]
+}
+
+local mjolnir_mod_name = "mjolnir._asm.applistener"
+local c_library = "internal"
+
+-- integration with C functions ------------------------------------------
+
+if c_library then
+	for i,v in pairs(require(mjolnir_mod_name.."."..c_library)) do module[i] = v end
+end
+
+-- private variables and methods -----------------------------------------
+
+local function wrap(fn)
+  return function()
+    if fn then
+      local ok, err = xpcall(fn, debug.traceback)
+      if not ok then mjolnir.showerror(err) end
+    end
+  end
+end
+
+-- Public interface ------------------------------------------------------
+
+--- mjolnir._asm.applistener.new(fn(name, object, userinfo)) -> applistener
+--- Constructor
+--- Registers a listener function for inter-app notifications.
+function module.new(fn)
+  local _fn = wrap(fn)
+
+  local t = module._new(_fn)
+  return t
+end
+
+-- Return Module Object --------------------------------------------------
+
+return module
+
---
+++ applistener/internal.m
@@ -0,0 +1,131 @@
+#import <Cocoa/Cocoa.h>
+#import <lauxlib.h>
+
+void mjolnir_push_luavalue_for_nsobject(lua_State* L, id obj) {
+    if (obj == nil || [obj isEqual: [NSNull null]]) {
+        lua_pushnil(L);
+    }
+    else if ([obj isKindOfClass: [NSDictionary class]]) {
+        lua_newtable(L);
+        NSDictionary* dict = obj;
+        
+        for (id key in dict) {
+            mjolnir_push_luavalue_for_nsobject(L, key);
+            mjolnir_push_luavalue_for_nsobject(L, [dict objectForKey:key]);
+            lua_settable(L, -3);
+        }
+    }
+    else if ([obj isKindOfClass: [NSNumber class]]) {
+        if (obj == (id)kCFBooleanTrue)
+            lua_pushboolean(L, YES);
+        else if (obj == (id)kCFBooleanFalse)
+            lua_pushboolean(L, NO);
+        else
+            lua_pushnumber(L, [(NSNumber*)obj doubleValue]);
+    }
+    else if ([obj isKindOfClass: [NSString class]]) {
+        NSString* string = obj;
+        lua_pushstring(L, [string UTF8String]);
+    }
+    else if ([obj isKindOfClass: [NSDate class]]) {
+        // not used for json, only in applistener; this should probably be moved to helpers
+        NSDate* string = obj;
+        lua_pushstring(L, [[string description] UTF8String]);
+    }
+    else if ([obj isKindOfClass: [NSArray class]]) {
+        lua_newtable(L);
+        
+        int i = 0;
+        NSArray* list = obj;
+        
+        for (id item in list) {
+            mjolnir_push_luavalue_for_nsobject(L, item);
+            lua_rawseti(L, -2, ++i);
+        }
+    }
+}
+
+@interface MjolnirAppListenerClass : NSObject
+@property lua_State* L;
+@property int fn;
+@property int ref;
+@end
+@implementation MjolnirAppListenerClass
+- (void) heard:(NSNotification*)note {
+    lua_State* L = self.L;
+    lua_rawgeti(L, LUA_REGISTRYINDEX, self.fn);
+    
+    lua_pushstring(L, [[note name] UTF8String]);
+    mjolnir_push_luavalue_for_nsobject(L, [note object]);
+    mjolnir_push_luavalue_for_nsobject(L, [note userInfo]);
+    lua_call(L, 3, 0);
+}
+@end
+
+static NSMutableIndexSet* listenerhandlers;
+
+static int store_applistener(lua_State* L, int idx) {
+    lua_pushvalue(L, idx);
+    int x = luaL_ref(L, LUA_REGISTRYINDEX);
+    [listenerhandlers addIndex: x];
+    return x;
+}
+
+static void remove_applistener(lua_State* L, int x) {
+    luaL_unref(L, LUA_REGISTRYINDEX, x);
+    [listenerhandlers removeIndex: x];
+}
+
+// mjolnir._asm.applistener.new(fn(notification)) -> applistener
+// Registers a listener function for inter-app notifications.
+static int applistener_new(lua_State* L) {
+    luaL_checktype(L, 1, LUA_TFUNCTION);
+    
+    MjolnirAppListenerClass* listener = [[MjolnirAppListenerClass alloc] init];
+    listener.L = L;
+    
+    lua_pushvalue(L, 1);
+    listener.fn = luaL_ref(L, LUA_REGISTRYINDEX);
+    
+    void** ud = lua_newuserdata(L, sizeof(id*));
+    *ud = (__bridge void*)listener;
+    
+    luaL_getmetatable(L, "applistener");
+    lua_setmetatable(L, -2);
+    
+    return 1;
+}
+
+/// mjolnir._asm.applistener:start()
+/// Method
+/// Starts listening for notifications.
+static int applistener_start(lua_State* L) {
+    MjolnirAppListenerClass* applistener = (__bridge MjolnirAppListenerClass*)(*(void**)luaL_checkudata(L, 1, "applistener"));
+    [[NSDistributedNotificationCenter defaultCenter] addObserver:applistener selector:@selector(heard:) name:nil object:nil];
+    applistener.ref = store_applistener(L, 1);
+    return 0;
+}
+
+/// mjolnir._asm.applistener:stop()
+/// Method
+/// Stops listening for notifications.
+static int applistener_stop(lua_State* L) {
+    MjolnirAppListenerClass* applistener = (__bridge MjolnirAppListenerClass*)(*(void**)luaL_checkudata(L, 1, "applistener"));
+    [[NSDistributedNotificationCenter defaultCenter] removeObserver:applistener];
+    remove_applistener(L, applistener.ref);
+    return 0;
+}
+
+static const luaL_Reg applistenerlib[] = {
+    {"_new", applistener_new},
+    {"start", applistener_start},
+    {"stop", applistener_stop},
+    {"__gc", applistener_stop},
+    {NULL, NULL}
+};
+
+int luaopen_mjolnir__asm_applistener_internal(lua_State* L) {
+    luaL_newlib(L, applistenerlib);
+
+    return 1;
+}
