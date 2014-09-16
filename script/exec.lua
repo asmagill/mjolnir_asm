973b6c6f6869ab9b9348108b54f0ac29cbd1c3bb
asmagill <asmagill@users.noreply.github.com>
2014-09-16 01:42:40 -0500

    shouldn't have been published

--- script/exec.lua
+++
@@ -1,8 +0,0 @@
---- hydra.exec(command) -> string
---- Runs a shell function and returns stdout as a string (may include trailing newline).
-function hydra.exec(command)
-  local f = io.popen(command)
-  local str = f:read('*a')
-  f:close()
-  return str
-end
