0b8d790ead0a20270ffef7244471b4eb82447175
asmagill <asmagill@users.noreply.github.com>
2014-10-02 15:53:14 -0500

    deprecating in favor of better separation, but just in case it makes it another round before done...

--- data/init.lua
+++ data/init.lua
@@ -5,7 +5,7 @@ local module = {
     _URL         = 'https://github.com/asmagill/mjolnir_asm.data',
     _LICENSE     = [[ See README.md ]]
     _DESCRIPTION = [[
-    
+
 --- === mjolnir._asm.data ===
 ---
 --- Home: https://github.com/asmagill/mjolnir_asm.data
@@ -57,8 +57,7 @@ end
 
 --- mjolnir._asm.data.applekeys[...]
 --- Variable
---- Array of symbols representing special keys in the mac environment, as per
---- http://macbiblioblog.blogspot.com/2005/05/special-key-symbols.html.
+--- Array of symbols representing special keys in the mac environment, as per http://macbiblioblog.blogspot.com/2005/05/special-key-symbols.html.
 module.applekeys = {
     ["escape"] = "⎋",
     ["tab"] = "⇥",
@@ -99,6 +98,44 @@ module.applekeys = {
     ["eject"] = "⏏",
     ["power"] = "⌽",
 }
+
+--- mjolnir._asm.data.hexdump(string [, count]) -> string
+--- Function
+--- Treats the input string as a binary blob and returns a prettied up hex dump of it's contents. By default, a newline character is inserted after every 16 bytes, though this can be changed by also providing the optional count argument.
+
+module.hexdump = function(stuff, linemax)
+	local ascii = ""
+	local count = 0
+	local linemax = tonumber(linemax) or 16
+	local buffer = ""
+	local rb = ""
+	local offset = math.floor(math.log(#stuff,16)) + 1
+	offset = offset + (offset % 2)
+
+	local formatstr = "%0"..tostring(offset).."x : %-"..tostring(linemax * 3).."s : %s"
+
+	for c in string.gmatch(tostring(stuff), ".") do
+		buffer = buffer..string.format("%02X ",string.byte(c))
+		-- using string.gsub(c,"%c",".") didn't work in mjolnir, but
+		-- I didn't dig any deeper -- this works.
+		if string.byte(c) < 32 or string.byte(c) > 126 then
+		    ascii = ascii.."."
+		else
+		    ascii = ascii..c
+		end
+		count = count + 1
+		if count % linemax == 0 then
+			rb = rb .. string.format(formatstr, count - linemax, buffer, ascii) .. "\n"
+			buffer=""
+			ascii=""
+		end
+	end
+	if count % linemax ~= 0 then
+		rb = rb .. string.format(formatstr, count - (count % linemax), buffer, ascii) .. "\n"
+	end
+	return rb
+end
+
 
 -- Return Module Object --------------------------------------------------
 
