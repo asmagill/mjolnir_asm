72b9bd9df6ce2cbe826f7d369911621c34ce3662
asmagill <asmagill@users.noreply.github.com>
2014-10-04 04:28:13 -0500

    brightness now works

---
+++ sys/brightness/init.lua
@@ -0,0 +1,23 @@
+--- === mjolnir._asm.sys.brightness ===
+---
+--- Home: https://github.com/asmagill/mjolnir_asm.sys
+---
+--- Functions for manipulating display brightness.
+---
+--- This module is based primarily on code from the previous incarnation of Mjolnir by [Steven Degutis](https://github.com/sdegutis/).
+
+require("mjolnir._asm")
+local module = require("mjolnir._asm.sys.brightness.internal")
+
+-- private variables and methods -----------------------------------------
+
+local check_list = {}
+
+-- Public interface ------------------------------------------------------
+
+-- Return Module Object --------------------------------------------------
+
+return module
+
+
+
